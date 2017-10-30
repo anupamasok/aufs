3a38a80dd20926943376660b52b0606ba1a66ad0
anupamasok <anupamasok007@gmail.com>
2017-10-30 18:53:51 +0530

    Update file.c

--- kern/file.c
+++ kern/file.c
@@ -2,7 +2,7 @@
 
 const struct file_operations aufs_file_ops = {
 	.llseek = generic_file_llseek,
-	.read = new_sync_read,
+	.read = new_sync_read,	/* A Possible error, leading compile Problems. Can be fixed as  .read = NULL, */
 	.read_iter = generic_file_read_iter,
 	.mmap = generic_file_mmap,
 	.splice_read = generic_file_splice_read
